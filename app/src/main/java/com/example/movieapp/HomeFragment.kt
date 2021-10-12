348246f0d3f5772399faeca29aea281d04ba6755
fauzanebd <fauzanebd@gmail.com>
2021-10-12 22:48:35 +0700

    adding movie list, movie details

--- app/src/main/java/com/example/movieapp/HomeFragment.kt
+++ app/src/main/java/com/example/movieapp/HomeFragment.kt
@@ -1,10 +1,36 @@
 package com.example.movieapp
 
+import android.app.Activity
+import android.content.ContentValues.TAG
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.WindowManager
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
+import android.util.Log
+import android.view.Window;
+import android.widget.ImageView;
+import android.widget.LinearLayout
+import android.widget.Toast
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.fragment_home.view.*
+
+//import com.azhar.moviedb.fragment.FragmentFavorite;
+//import com.azhar.moviedb.fragment.FragmentMovie;
+//import com.azhar.moviedb.fragment.FragmentTV;
+//import com.azhar.moviedb.R;
+//import com.azhar.moviedb.utils.BottomBarBehavior;
+import com.gauravk.bubblenavigation.BubbleNavigationLinearView;
+import com.gauravk.bubblenavigation.listener.BubbleNavigationChangeListener;
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -21,6 +47,11 @@ class HomeFragment : Fragment() {
     private var param1: String? = null
     private var param2: String? = null
 
+    private lateinit var viewOfLayout: View
+//    private lateinit var popularMovies: RecyclerView
+    private lateinit var popularMoviesAdapter: MoviesAdapter
+    private lateinit var topRatedMoviesAdapter: MoviesAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
@@ -34,7 +65,56 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        viewOfLayout = inflater.inflate(R.layout.fragment_home, container, false)
+        viewOfLayout.popular_movies.layoutManager = LinearLayoutManager(
+            activity,
+            LinearLayoutManager.HORIZONTAL,
+            false
+        )
+        fun showMovieDetails(movie: Movie) {
+            val intent = Intent(activity, MovieDetailsActivity::class.java)
+            intent.putExtra(MOVIE_BACKDROP, movie.backdropPath)
+            intent.putExtra(MOVIE_POSTER, movie.posterPath)
+            intent.putExtra(MOVIE_TITLE, movie.title)
+            intent.putExtra(MOVIE_RATING, movie.rating)
+            intent.putExtra(MOVIE_RELEASE_DATE, movie.releaseDate)
+            intent.putExtra(MOVIE_OVERVIEW, movie.overview)
+            startActivity(intent)
+        }
+        viewOfLayout.top_rated_movies.layoutManager = LinearLayoutManager(
+            activity,
+            LinearLayoutManager.HORIZONTAL,
+            false
+        )
+        popularMoviesAdapter = MoviesAdapter(listOf()) {movie -> showMovieDetails(movie)}
+        topRatedMoviesAdapter = MoviesAdapter(listOf()) {movie -> showMovieDetails(movie)}
+        viewOfLayout.popular_movies.adapter = popularMoviesAdapter
+        viewOfLayout.top_rated_movies.adapter = topRatedMoviesAdapter
+
+
+
+        fun onError() {
+            Toast.makeText(activity, getString(R.string.error_fetch_movies), Toast.LENGTH_SHORT)
+                .show()
+        }
+
+        MoviesRepository.getPopularMovies(
+            onSuccess = ::onPopularMoviesFetched,
+            onError = ::onError
+        )
+        MoviesRepository.getTopRatedMovies(
+            onSuccess = ::onTopRatedMoviesFetched,
+            onError = ::onError
+        )
+        Log.d(TAG, "fragment home berhasil")
+        return viewOfLayout
+    }
+
+    private fun onPopularMoviesFetched(movies: List<Movie>) {
+        popularMoviesAdapter.updateMovies(movies)
+    }
+    private fun onTopRatedMoviesFetched(movies: List<Movie>) {
+        topRatedMoviesAdapter.updateMovies(movies)
     }
 
     companion object {
@@ -48,12 +128,24 @@ class HomeFragment : Fragment() {
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
-        fun newInstance(param1: String, param2: String) =
+        fun newInstance(param1: String, param2: String) {
             HomeFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
                 }
             }
+        }
+
+        fun setWindowFlag(activity: Activity, bits: Int, on: Boolean) {
+            val window: Window = activity.window
+            val winParams: WindowManager.LayoutParams = window.getAttributes()
+            if (on) {
+                winParams.flags = winParams.flags or bits
+            } else {
+                winParams.flags = winParams.flags and bits.inv()
+            }
+            window.setAttributes(winParams)
+        }
     }
 }
--- app/src/main/java/com/example/movieapp/MainActivity.kt
+++ app/src/main/java/com/example/movieapp/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.example.movieapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
@@ -10,9 +14,22 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportActionBar?.hide()
+        fun onError() {
+            Toast.makeText(this, getString(R.string.error_fetch_movies), Toast.LENGTH_SHORT).show()
+        }
+
+        fun onPopularMoviesFetched(movies: List<Movie>) {
+            Log.d("MainActivity", "Movies: $movies")
+        }
+        MoviesRepository.getPopularMovies(
+            onSuccess = ::onPopularMoviesFetched,
+            onError = ::onError
+        )
 
         val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
         val navController = findNavController(R.id.fragment)
@@ -20,7 +37,6 @@ class MainActivity : AppCompatActivity() {
         bottomNavigationView.setupWithNavController(navController)
         val appBarConfiguration = AppBarConfiguration(setOf(R.id.homeFragment, R.id.aboutFragment))
         setupActionBarWithNavController(navController, appBarConfiguration)
-
 
     }
 
